bfb8aee85dd5464f55d3f2528cae302cba60bd9c
thongth <thongth@THONGTH>
2020-10-16 15:14:21 +0700

    update

--- myapp/app.js
+++ myapp/app.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var username = null;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({
+    extended: true     // de doc duoc json request can import cai nay
+}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    username = null;
+    res.redirect('/login');
+});
 
 app.get('/login', (req, res) => {
-    res.send('Hello World!')
-  })
+    res.render('users/login', {
+        title: 'login form'
+    });
+});
+
+app.get('/home', (req, res) => {    
+    if (username == null) {
+        res.redirect('/login');
+    } else {
+        res.render('index', {
+            title: 'Home page',
+            value: username
+        });
+    }
+});
+
+app.post('/login', function (req, res) {
+    username = req.body.username;
+    res.redirect('/home');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
