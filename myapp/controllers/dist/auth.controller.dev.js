c3d9b2c0277c83653060eb0f56291b814e05cb6e
thongth <thongth@THONGTH>
2020-10-20 11:15:23 +0700

    update layout

---
+++ myapp/controllers/dist/auth.controller.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports.login = function (req, res) {
+  res.render('users/login', {
+    title: 'login form',
+    layout: '../views/layouts/empty'
+  });
+};
+
+module.exports.postLogin = function (req, res) {
+  res.redirect('/home');
+};
