3115968d4eeccae9288cdf6b8b257a6c55329600
thongth <thongth@THONGTH>
2020-10-20 15:55:20 +0700

    update menu

--- myapp/public/scripts/dist/menu.dev.js
+++ myapp/public/scripts/dist/menu.dev.js
@@ -11,8 +11,19 @@ $(document).ready(function () {
     // $('#my-sidebar').toggleClass('my-sidebar', 1000)
     if ($('#my-setting').is(":hidden")) {
       $('#my-setting').slideDown("slow");
-    } else {
-      $('#my-setting').hide();
+    } else {//$('#my-setting').hide();
+    }
+  });
+  $(document).click(function (e) {
+    var containermenu = $('.mysetting');
+
+    if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
+      //Đúng là bấm chuột ngoài menu
+      var isopened = containermenu.find('#my-setting').css("display"); //Ẩn menu đang mở
+
+      if (isopened == 'block') {
+        containermenu.find('#my-setting').slideToggle(500);
+      }
     }
   });
 });
--- myapp/public/scripts/menu.js
+++ myapp/public/scripts/menu.js
@@ -9,7 +9,21 @@ $(document).ready(function () {
         if ($('#my-setting').is(":hidden")) {
             $('#my-setting').slideDown("slow")
         } else {
-            $('#my-setting').hide();
+            //$('#my-setting').hide();
+        }
+    });
+
+    $(document).click(function (e) {
+        var containermenu = $('.mysetting');
+        if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
+            //Đúng là bấm chuột ngoài menu
+            var isopened =
+                containermenu.find('#my-setting').css("display");
+
+            //Ẩn menu đang mở
+            if (isopened == 'block') {
+                containermenu.find('#my-setting').slideToggle(500);
+            }
         }
     });
 });
