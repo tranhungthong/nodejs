b099368762d23958ee6a1eaa9dc6803fef2829cf
thongth <thongth@THONGTH>
2020-10-20 15:13:34 +0700

    update layout

---
+++ myapp/public/scripts/dist/menu.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$(document).ready(function () {
+  $('#btnCollapse').click(function () {
+    // $('#my-sidebar').toggleClass('my-sidebar', 1000)
+    $('#my-sidebar').animate({
+      width: 'toggle'
+    }, 350);
+  });
+  $('#btnavatar').click(function () {
+    // $('#my-sidebar').toggleClass('my-sidebar', 1000)
+    if ($('#my-setting').is(":hidden")) {
+      $('#my-setting').slideDown("slow");
+    } else {
+      $('#my-setting').hide();
+    }
+  });
+});
