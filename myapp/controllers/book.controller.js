1884a18865a65a5ca6062efb727113f5509af1a5
thongth <thongth@THONGTH>
2020-10-21 15:27:21 +0700

    update

--- myapp/controllers/book.controller.js
+++ myapp/controllers/book.controller.js
@@ -18,3 +18,30 @@ module.exports.index = function (req, res) {
 
 
 };
+
+module.exports.search = function (req, res) {
+    // get data
+    var input = '^.*' + req.body.search + '.*';
+
+    Book.find({
+        $or: [
+            { title: { $regex: new RegExp(input, "i") } },
+            { author: { $regex: new RegExp(input, "i") } }
+        ]
+    }, function (err, data) {
+        console.log(data);
+        if (data.length > 0) {
+            res.render('books/index', {
+                books: data
+            });
+
+            return;
+        }
+
+        res.render('books/index', {
+            books: null
+        });
+    });
+
+
+};
--- myapp/controllers/dist/book.controller.dev.js
+++ myapp/controllers/dist/book.controller.dev.js
@@ -16,3 +16,32 @@ module.exports.index = function (req, res) {
     res.render('books/index');
   });
 };
+
+module.exports.search = function (req, res) {
+  // get data
+  var input = '^.*' + req.body.search + '.*';
+  Book.find({
+    $or: [{
+      title: {
+        $regex: new RegExp(input, "i")
+      }
+    }, {
+      author: {
+        $regex: new RegExp(input, "i")
+      }
+    }]
+  }, function (err, data) {
+    console.log(data);
+
+    if (data.length > 0) {
+      res.render('books/index', {
+        books: data
+      });
+      return;
+    }
+
+    res.render('books/index', {
+      books: null
+    });
+  });
+};
