c718035fb4b3e239af2b2f33aea7bbbc0ce65ae7
thongth <thongth@THONGTH>
2020-10-21 14:58:25 +0700

    update

--- myapp/controllers/dist/book.controller.dev.js
+++ myapp/controllers/dist/book.controller.dev.js
@@ -1,5 +1,18 @@
 "use strict";
 
+var Book = require('../models/book.model');
+
 module.exports.index = function (req, res) {
-  res.render('books/index');
+  // get data
+  Book.find({}, function (err, data) {
+    if (data.length > 0) {
+      console.log(data);
+      res.render('books/index', {
+        books: data
+      });
+      return;
+    }
+
+    res.render('books/index');
+  });
 };
---
+++ myapp/models/dist/book.model.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  summary: String,
+  ISBN: String,
+  genre: String,
+  img_cover: String,
+  create_date: String,
+  create_by: String,
+  update_date: String,
+  update_by: String
+});
+var Book = mongoose.model('Book', userSchema, 'Book');
+module.exports = Book;
