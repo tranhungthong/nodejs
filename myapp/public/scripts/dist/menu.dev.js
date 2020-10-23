3ed782765cd2bc82d3e242750064729f99f96763
thongth <thongth@THONGTH>
2020-10-23 11:08:41 +0700

    update

--- myapp/public/scripts/dist/menu.dev.js
+++ myapp/public/scripts/dist/menu.dev.js
@@ -1,29 +1,36 @@
 "use strict";
 
 $(document).ready(function () {
+  var OriginalWidth = 256;
   $('#btnCollapse').click(function () {
     // $('#my-sidebar').toggleClass('my-sidebar', 1000)
     $('#my-sidebar').animate({
       width: 'toggle'
-    }, 350);
+    }, 350); // width = $('#my-sidebar').width();
+    // if (OriginalWidth == width) {
+    //     $('#my-sidebar').animate({ width: '50' }, 'slow');
+    // }
+    // else {
+    //     $('#my-sidebar').animate({ width: OriginalWidth }, 'slow');
+    // }
   });
-  $('#btnavatar').click(function () {
-    // $('#my-sidebar').toggleClass('my-sidebar', 1000)
-    if ($('#my-setting').is(":hidden")) {
-      $('#my-setting').slideDown("slow");
-    } else {//$('#my-setting').hide();
-    }
-  });
-  $(document).click(function (e) {
-    var containermenu = $('.mysetting');
+});
+$('#btnavatar').click(function () {
+  // $('#my-sidebar').toggleClass('my-sidebar', 1000)
+  if ($('#my-setting').is(":hidden")) {
+    $('#my-setting').slideDown("slow");
+  } else {//$('#my-setting').hide();
+  }
+});
+$(document).click(function (e) {
+  var containermenu = $('.mysetting');
 
-    if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
-      //Đúng là bấm chuột ngoài menu
-      var isopened = containermenu.find('#my-setting').css("display"); //Ẩn menu đang mở
+  if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
+    //Đúng là bấm chuột ngoài menu
+    var isopened = containermenu.find('#my-setting').css("display"); //Ẩn menu đang mở
 
-      if (isopened == 'block') {
-        containermenu.find('#my-setting').slideToggle(500);
-      }
+    if (isopened == 'block') {
+      containermenu.find('#my-setting').slideToggle(500);
     }
-  });
+  }
 });
--- myapp/public/scripts/menu.js
+++ myapp/public/scripts/menu.js
@@ -1,29 +1,38 @@
 $(document).ready(function () {
+    var OriginalWidth = 256;
     $('#btnCollapse').click(function () {
         // $('#my-sidebar').toggleClass('my-sidebar', 1000)
         $('#my-sidebar').animate({ width: 'toggle' }, 350);
-    });
 
-    $('#btnavatar').click(function () {
-        // $('#my-sidebar').toggleClass('my-sidebar', 1000)
-        if ($('#my-setting').is(":hidden")) {
-            $('#my-setting').slideDown("slow")
-        } else {
-            //$('#my-setting').hide();
+        // width = $('#my-sidebar').width();
+        // if (OriginalWidth == width) {
+        //     $('#my-sidebar').animate({ width: '50' }, 'slow');
+        // }
+        // else {
+        //     $('#my-sidebar').animate({ width: OriginalWidth }, 'slow');
+        // }
+    });
+});
+
+$('#btnavatar').click(function () {
+    // $('#my-sidebar').toggleClass('my-sidebar', 1000)
+    if ($('#my-setting').is(":hidden")) {
+        $('#my-setting').slideDown("slow")
+    } else {
+        //$('#my-setting').hide();
+    }
+});
+
+$(document).click(function (e) {
+    var containermenu = $('.mysetting');
+    if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
+        //Đúng là bấm chuột ngoài menu
+        var isopened =
+            containermenu.find('#my-setting').css("display");
+
+        //Ẩn menu đang mở
+        if (isopened == 'block') {
+            containermenu.find('#my-setting').slideToggle(500);
         }
-    });
-
-    $(document).click(function (e) {
-        var containermenu = $('.mysetting');
-        if (!containermenu.is(e.target) && containermenu.has(e.target).length === 0) {
-            //Đúng là bấm chuột ngoài menu
-            var isopened =
-                containermenu.find('#my-setting').css("display");
-
-            //Ẩn menu đang mở
-            if (isopened == 'block') {
-                containermenu.find('#my-setting').slideToggle(500);
-            }
-        }
-    });
+    }
 });
