9c63b03e386762a54615f4508d23625c73381e5a
thongth <thongth@THONGTH>
2020-11-03 10:14:58 +0700

    update

--- myapp/controllers/dist/weather.controller.dev.js
+++ myapp/controllers/dist/weather.controller.dev.js
@@ -1,6 +1,32 @@
 "use strict";
 
+var weatherApi = require('../weatherApiCommon');
+
 module.exports.index = function (req, res) {
+  console.log('begin call api');
+
+  var asyncApiCall = function asyncApiCall() {
+    var res;
+    return regeneratorRuntime.async(function asyncApiCall$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return regeneratorRuntime.awrap(weatherApi.GET('hanoi', 10, 'metric or imperial'));
+
+          case 2:
+            res = _context.sent;
+            console.log(res.data);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
+  };
+
+  asyncApiCall();
   res.render('weathers/index', {
     title: 'weather',
     errors: null
--- myapp/controllers/weather.controller.js
+++ myapp/controllers/weather.controller.js
@@ -1,6 +1,17 @@
+const weatherApi = require('../weatherApiCommon')
+
 module.exports.index = function (req, res) {
+    console.log('begin call api')
+    const asyncApiCall = async () => {
+        const res = await weatherApi.GET('hanoi', 10, 'metric or imperial');
+        console.log(res.data)
+    }
+
+    asyncApiCall();
+
     res.render('weathers/index', {
         title: 'weather',
         errors: null
     });
 };
+
